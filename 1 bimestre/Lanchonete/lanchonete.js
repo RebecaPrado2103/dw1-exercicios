eb532de87a955127886b53994e6d408850a27cd9
Rebeca Prado da Silva <rebecaprado@alunos.utfpr.edu.br>
2026-04-16 08:58:42 -0300

    alterações em lanchonete.js

--- 1 bimestre/Lanchonete/lanchonete.js
+++ 1 bimestre/Lanchonete/lanchonete.js
@@ -1,24 +1,31 @@
 function funcaoCalcular(){
-    let cqb = parseInt(document.getElementById("inputCQB").value) || 0;
-    let cqd = parseInt(document.getElementById("inputCQD").value)|| 0;
-    let xs = parseInt(document.getElementById("inputXS").value)|| 0;
-    let refri = parseInt(document.getElementById("inputRefri").value)|| 0;
-    let r = parseInt(document.getElementById("inputR").value)|| 0;
+    let cqb = parseFloat(document.getElementById("inputCQB").value) || 0;
+    let cqd = parseFloat(document.getElementById("inputCQD").value)|| 0;
+    let xs = parseFloat(document.getElementById("inputXS").value)|| 0;
+    let refri = parseFloat(document.getElementById("inputRefri").value)|| 0;
+    let r = parseFloat(document.getElementById("inputR").value)|| 0;
 
     let footer = document.getElementById("footer");
     let mensagem = document.getElementById("mensagem");
-
-    let total = (22*cqb) + (27*cqd) + (29*xs) + (5*refri) + (8*r);
 
     if(cqb<0 || cqd<0 || xs<0 || refri<0 || r<0){
         footer.classList.add("erro");
         mensagem.innerHTML = "Os valores não podem ser negativos";
         document.getElementById("resposta").innerHTML = "-";
         return;
-    }else{
-        footer.classList.remove("erro");
-        mensagem.innerHTML = "Cálculo realizado com sucesso";
     }
+
+    if(!Number.isInteger(cqb) || !Number.isInteger(cqd) || !Number.isInteger(xs) || !Number.isInteger(refri) || !Number.isInteger(r)){
+        footer.classList.add("erro");
+        mensagem.innerHTML = "Os valores precisam ser inteiros";
+        document.getElementById("resposta").innerHTML = "-";
+        return;
+    }
+
+    let total = (22*cqb) + (27*cqd) + (29*xs) + (5*refri) + (8*r);
+
+    footer.classList.remove("erro");
+    mensagem.innerHTML = "Cálculo realizado com sucesso";
 
     document.getElementById("resposta").innerHTML = total;
 }
